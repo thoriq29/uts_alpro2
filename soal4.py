97056d3d3796be7febbf787ee48d3b7e56b5605c
thoriq29 <oiq2908@gmail.com>
2020-05-01 23:37:53 +0700

    refactor code

--- soal4.py
+++ soal4.py
@@ -4,15 +4,19 @@
 """
 
 def cariLuasSegitiga():
+    print("Aplikasi mencari luas segitiga")
     while True:
         try:
-            alas = int(input("Masukkan Alas = "))
-            tinggi = int(input("Masukkan Tinggi = "))
+            alas = int(input("Silahkan Masukkan Alas Segitiga = "))
+            tinggi = int(input("Silahkan Masukkan Tinggi Segitiga = "))
             luas = int(0.5*alas*tinggi)
             print("Luas segitiganya adalah : "+ str(luas))
         except ValueError:
             print("Masukkan Angka saja!")
             continue
+        except KeyboardInterrupt:
+            print("\nProgram dibatalkan")
+            break
         else:
             return luas
             break
--- soal6.py
+++ soal6.py
@@ -8,13 +8,16 @@ def cariGenapGanjil():
   while True:
     try:
         inputan = int(input("Masukkan Angka = "))
-        if ( inputan%2 == 0 ):
-                    print ("Genap")
+        if (inputan % 2 == 0):
+            print ("Genap")
         else:
-                    print ("Ganjil")
+            print ("Ganjil")
     except ValueError:
         print("Masukkan Angka saja!")
         continue
+    except KeyboardInterrupt:
+        print("\nProgram dibatalkan")
+        break
     else:
         return inputan
         break
